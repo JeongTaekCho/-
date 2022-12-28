7f7529faa591761e3244bd3020f9c8a6f14a16e1
JeongTaekCho <cjt3591@gmail.com>
2022-12-28 20:02:29 +0900

    [FEATURE] 물품등록 기능

--- src/components/products/write/write.container.tsx
+++ src/components/products/write/write.container.tsx
@@ -1,9 +1,25 @@
+import { useMutation } from "@apollo/client";
 import { useState } from "react";
 import ProductWriteUi from "./write.presenter";
+import { CREATE_USED_ITEM } from "./write.queries";
 
 const ProductWrite = () => {
   const [prevImage, setPrevImage] = useState([]);
   const [uploadImage, setUploadImage] = useState([]);
+
+  const [createBoard] = useMutation(CREATE_USED_ITEM);
+
+  const onClickSubmit = () => {
+    try {
+      createBoard({
+        variables: {},
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    }
+  };
 
   const onChangeFiles = (event) => {
     console.log(event.target.files);
